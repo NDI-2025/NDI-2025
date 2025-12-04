0c31235cc330e7e302866230cd62cca18df7ea92
Estéban DESESSARD <carteesde@gmail.com>
2025-12-04 21:51:00 +0100

    Création VoiceAuth

--- NDI-2025/routes/web.php
+++ NDI-2025/routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\VoiceAuth;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/voice', [VoiceAuth::class, 'showForm'])->name('voice.form');
+Route::post('/voice/register', [VoiceAuth::class, 'register'])->name('voice.register');
+Route::post('/voice/login', [VoiceAuth::class, 'login'])->name('voice.login');
